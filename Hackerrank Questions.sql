8a0705ee5d8d3504774aa7d6a34d00dbb253d881
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-06 18:28:06 +0530

    Update Hackerrank Questions.sql

--- Hackerrank Questions.sql
+++ Hackerrank Questions.sql
@@ -210,4 +210,15 @@ FROM occupations
 GROUP BY occupation
 ORDER BY COUNT(occupation), occupation
 
+/* (WEATHER OBSERATION STATION 18)
+Consider  and  to be two points on a 2D plane.
+ happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.    */
 
+-- For two points A(x1, y1) and B(x2, y2), Manhattan distance is |x1-x2|+|y1-y2|  or The distance between two points measured along axes at right angles
+
+SELECT ROUND(ABS(MIN(lat_n)-MAX(lat_n))+ABS(MIN(long_w)-MAX(long_w)),4)
+FROM station
