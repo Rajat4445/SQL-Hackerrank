f06a3d5412e69c72789d92340d8e567d4f9b1b07
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-06 18:00:47 +0530

    Update Hackerrank Questions.sql

--- Hackerrank Questions.sql
+++ Hackerrank Questions.sql
@@ -195,5 +195,19 @@ On city.countrycode = country.code
 WHERE city.id IS NOT NULL
 GROUP BY continent 
 
+/* (THE PADS)
+Generate the following two result sets:
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format: 
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+*/
+SELECT CONCAT(name,'(', SUBSTRING(occupation, 1,1),')')
+FROM occupations
+ORDER BY name;                                                  -- There are two separate queries as you can observe
+SELECT CONCAT('There are a total of ', COUNT(occupation),' ', lower(occupation),'s.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(occupation), occupation
 
 
