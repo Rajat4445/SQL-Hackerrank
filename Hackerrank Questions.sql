43a392f89bb7ea306523acaaa47aa12dc944848b
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-06 18:37:21 +0530

    Update Hackerrank Questions.sql

--- Hackerrank Questions.sql
+++ Hackerrank Questions.sql
@@ -222,3 +222,12 @@ Query the Manhattan Distance between points p1 and p2 and round it to a scale of
 
 SELECT ROUND(ABS(MIN(lat_n)-MAX(lat_n))+ABS(MIN(long_w)-MAX(long_w)),4)
 FROM station
+
+/*
+Consider p1(a,c) and p2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points p1  and p2  and format your answer to display 4 decimal digits.
+*/
+
+SELECT ROUND(SQRT(POWER(MIN(lat_n)-MAX(lat_n), 2)+POWER(MIN(long_w)-MAX(long_w), 2)), 4)
+FROM station          -- As ED -->      h^2 = p^2 + b^2
+
